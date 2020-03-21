a49f4ba786227b2b8fc869ebc33920e8a2872218
Jakub Sapko <jakub.sapko1@gmail.com>
2020-03-21 14:49:16 +0100

    initialized work on nevilles algorithm

---
+++ Exercises_from_webpage/week6/nevilles_algorithm.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+double neville(const double xa[], const double ya[], size_t size, double x){
+
+}
+
+int main(){
+
+	double x[]{-1, 0, 1, 2};
+	double f[]{-5, -1, -1, 1};
+	cout << neville(x, f, 4, 3) << endl;
+
+return 0;
+}
