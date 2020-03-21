5148bf811f33aefab399c8e0572f408101b12c2d
Jakub Sapko <jakub.sapko1@gmail.com>
2020-03-21 22:30:20 +0100

    coded 1st version of neville's algo

--- Exercises_from_webpage/week6/nevilles_algorithm.cpp
+++ Exercises_from_webpage/week6/nevilles_algorithm.cpp
@@ -3,15 +3,34 @@
 
 using namespace std;
 
-double neville(const double xa[], const double ya[], size_t size, double x){
-
+void neville(const double xa[], const double ya[], size_t size, double x){
+	double Q[size][size];
+	for (int i = 0; i < size; i++){
+		for (int j = 0; j < size; j++){
+			Q[i][j] = 0.0;
+		}
+	}
+	for (int i = 0; i < size; i++){
+		Q[i][0] = ya[i];
+	}
+	for (int i = 1; i < size; i++){
+		for (int j = 1; j<= i; j++){
+			Q[i][j] = ((x - xa[i-j])*(Q[i][j-1])-(x-xa[i])*(Q[i-1][j-1]))/(xa[i]-xa[i-j]);
+	}
+}
+	for ( int i = 0; i< size; i++){
+		for (int j = 0; j< size; j++){
+			cout << Q[i][j];
+		}
+	cout << endl;
+	}
 }
 
 int main(){
 
-	double x[]{-1, 0, 1, 2};
-	double f[]{-5, -1, -1, 1};
-	cout << neville(x, f, 4, 3) << endl;
+	double x[]{-1, 0, 1, 2}; // arguments
+	double f[]{-5, -1, -1, 1}; //values for given arguments
+	neville(x, f, 4, 3);
 
 return 0;
 }
